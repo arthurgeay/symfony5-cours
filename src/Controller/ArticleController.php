500938482c1aff27dd91707ec4c587afbf1eb847
Arthur Geay <arthurgeay.contact@gmail.com>
2020-02-22 04:50:07 +0100

    Création du template pour les articles

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -6,8 +6,9 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class ArticleController
+class ArticleController extends AbstractController
 {
     /**
      * @Route("/")
@@ -22,6 +23,9 @@ class ArticleController
      */
     public function show($slug = 'yeah')
     {
-        return new Response(sprintf("Voici l'article : %s", $slug));
+        $comments = ["C'est incroyable", "Super article", "Nul"];
+        return $this->render('article/show.html.twig', [
+            'title' => ucwords(str_replace('-', ' ', $slug)),
+            'comments' => $comments]);
     }
 }
