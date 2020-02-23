5ebaf9d2fe405a542316cdaf4abe884ad1e4c1e2
Arthur Geay <arthurgeay.contact@gmail.com>
2020-02-23 19:55:40 +0100

    Création de paramètres

--- src/Service/MarkdownHelper.php
+++ src/Service/MarkdownHelper.php
@@ -13,16 +13,22 @@ class MarkdownHelper
     private $cache;
     private $markdown;
     private $logger;
+    private $isDebug;
 
-    public function __construct(AdapterInterface $cache, MarkdownInterface $markdown, LoggerInterface $markdownLogger)
+    public function __construct(AdapterInterface $cache, MarkdownInterface $markdown, LoggerInterface $markdownLogger, bool $isDebug)
     {
         $this->cache = $cache;
         $this->markdown = $markdown;
         $this->logger = $markdownLogger;
+        $this->isDebug = $isDebug;
     }
 
     public function parse(string $source): string
     {
+        if($this->isDebug) {
+            return $this->markdown->transform($source);
+        }
+
         if(stripos($source, 'bacon') !== false) {
             $this->logger->info('Ils parlent encore de bacon');
         }
