f516a6e805746240e9c93798c4c1539c9242a5e1
arthurgeay <arthurgeay.contact@gmail.com>
2020-03-04 22:39:07 +0100

    Système d'électeur

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -62,13 +62,12 @@ class ArticleAdminController extends AbstractController
 
     /**
      * @Route("/admin/article/{id}/edit")
+     * @IsGranted("MANAGE", subject="article")
      */
     public function edit(Article $article)
     {
-        if($article->getAuthor() != $this->getUser() && !$this->isGranted('ROLE_ADMIN_ARTICLE')) {
-            throw $this->createAccessDeniedException('Accès refusé');
-        }
 
+        $this->denyAccessUnlessGranted('MANAGE', $article);
         dd($article);
     }
 }
