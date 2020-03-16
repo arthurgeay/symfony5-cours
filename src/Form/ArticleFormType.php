44ad848c64de5d0be3c852f0eb058fe5705c9c6d
Arthur Geay <arthurgeay.contact@gmail.com>
2020-03-16 12:18:30 +0100

    Ajout du type de champ

--- src/Form/ArticleFormType.php
+++ src/Form/ArticleFormType.php
@@ -11,6 +11,7 @@ namespace App\Form;
 
 use App\Entity\Article;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -20,7 +21,7 @@ class ArticleFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
+            ->add('title', TextType::class, ['help' => 'Le titre de votre article'])
             ->add('content')
             ;
     }
