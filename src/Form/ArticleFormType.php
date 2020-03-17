447d519122c7ea3068d5712ce862ba5ea9cec9e3
Arthur Geay <arthurgeay.contact@gmail.com>
2020-03-17 12:48:19 +0100

    Requête personnalisée EntityType

--- src/Form/ArticleFormType.php
+++ src/Form/ArticleFormType.php
@@ -11,6 +11,7 @@ namespace App\Form;
 
 use App\Entity\Article;
 use App\Entity\User;
+use App\Repository\UserRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
@@ -20,6 +21,17 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ArticleFormType extends AbstractType
 {
+
+    /**
+     * @var UserRepository
+     */
+    private $userRepository;
+
+    public function __construct(UserRepository $userRepository)
+    {
+
+        $this->userRepository = $userRepository;
+    }
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
@@ -36,7 +48,8 @@ class ArticleFormType extends AbstractType
                 'choice_label' => function(User $user) {
                     return sprintf('(%d) %s', $user->getId(), $user->getEmail());
                 },
-                'placeholder' => 'Choisir un auteur'
+                'placeholder' => 'Choisir un auteur',
+                'choices' => $this->userRepository->findAllEmailAlphabetical()
             ])
             ;
     }
