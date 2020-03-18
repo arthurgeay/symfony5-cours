398a1903d1b3a14e54d5d20dd5778b3ac0e2d23f
Arthur Geay <arthurgeay.contact@gmail.com>
2020-03-18 23:03:57 +0100

    Formulaire d'édition d'un article

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -47,12 +47,28 @@ class ArticleAdminController extends AbstractController
 
 
     /**
-     * @Route("/admin/article/{id}/edit")
+     * @Route("/admin/article/{id}/edit", name="edit_article")
      * @IsGranted("MANAGE", subject="article")
      */
-    public function edit(Article $article)
+    public function edit(Article $article, Request $request, EntityManagerInterface $em)
     {
-        dd($article);
+        $form = $this->createForm(ArticleFormType::class, $article);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            //$article = $form->getData();
+
+            //$em->persist($article);
+            $em->flush();
+
+            $this->addFlash('success', 'Article mis à jour');
+
+            return $this->redirectToRoute('edit_article', ['id' => $article->getId()]);
+        }
+
+        return $this->render('article_admin/edit.html.twig', [
+            'articleForm' => $form->createView()
+        ]);
     }
 
     /**
