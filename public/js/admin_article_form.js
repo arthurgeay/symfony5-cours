464e5a143174e3244bffb4b71727706e077b41b2
Arthur Geay <arthurgeay.contact@gmail.com>
2020-04-14 16:57:19 +0200

    Ajout d'évènement en JS pour gérer l'affichage des options

---
+++ public/js/admin_article_form.js
@@ -0,0 +1,22 @@
+const locationSelect = document.querySelector('.js-article-form-location');
+const specificLocationUrl = locationSelect.dataset.specificLocationUrl;
+
+const specificLocationTarget = document.querySelector('.js-specific-location-target');
+
+
+locationSelect.addEventListener('change', (e) => {
+    fetch(specificLocationUrl + `?location=${e.target.value}`)
+        .then(data => data.text())
+        .then(result => {
+            if(!result) {
+              const select = specificLocationTarget.children[0].querySelector('select');
+              specificLocationTarget.children[0].removeChild(select);
+              specificLocationTarget.classList.add('d-none');
+
+              return;
+            }
+
+            specificLocationTarget.innerHTML = result;
+            specificLocationTarget.classList.remove('d-none');
+        })
+});
